f16c64c86e4a26a01b83c308082b96a5a996da44
Hossam harraz <harrazhossam11@gmail.com>
2024-05-14 02:05:50 +0300

    show employees

--- resources/views/pages/employee/index.blade.php
+++ resources/views/pages/employee/index.blade.php
@@ -22,43 +22,41 @@ employees
           <table class="table align-items-center mb-0">
             <thead>
               <tr>
-                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Author</th>
-                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Function</th>
-                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
-                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Employed</th>
+                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Name</th>
+                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Age</th>
+                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Salary</th>
                 <th class="text-secondary opacity-7"></th>
               </tr>
             </thead>
             <tbody>
-        
+        @foreach ($employees as $employee)
+            
+       
               <tr>
                 <td>
                   <div class="d-flex px-2 py-1">
                     <div>
-                      <img src="../assets/img/team-4.jpg" class="avatar avatar-sm me-3" alt="user6">
+                      <img src="{{ $employee->img }}" class="avatar avatar-sm me-3" alt="user6">
                     </div>
                     <div class="d-flex flex-column justify-content-center">
-                      <h6 class="mb-0 text-sm">Miriam Eric</h6>
-                      <p class="text-xs text-secondary mb-0">miriam@creative-tim.com</p>
+                      <h6 class="mb-0 text-sm">{{ $employee->name }}</h6>
                     </div>
                   </div>
                 </td>
                 <td>
-                  <p class="text-xs font-weight-bold mb-0">Programtor</p>
-                  <p class="text-xs text-secondary mb-0">Developer</p>
+                  <p class="text-xs text-secondary mb-0">{{ $employee->age }}</p>
                 </td>
                 <td class="align-middle text-center text-sm">
-                  <span class="badge badge-sm bg-gradient-secondary">Offline</span>
+                  <span class="badge badge-sm bg-gradient-secondary">{{ $employee->salary }}</span>
                 </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">14/09/20</span>
-                </td>
+                
                 <td class="align-middle">
                   <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
                     Edit
                   </a>
                 </td>
               </tr>
+              @endforeach
             </tbody>
           </table>
         </div>
