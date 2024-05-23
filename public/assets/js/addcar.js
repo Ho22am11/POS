26f49b82d08cfa63a2c2e55c6e10d0dbc8a17d59
Hossam harraz <harrazhossam11@gmail.com>
2024-05-23 15:53:54 +0300

    button delete of prodect

--- public/assets/js/addcar.js
+++ public/assets/js/addcar.js
@@ -2,10 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.add-prodect-btn').forEach(function(button) {
         button.addEventListener('click', function(event) {
             event.preventDefault(); // Prevent the default link behavior
+            var id = this.getAttribute('data-id');
             var name = this.getAttribute('data-name');
             var amount = this.getAttribute('data-amount');
-            var html = `<tr> <td>${name}</td> <td><input type = "number" name="count" class="form-control" value="1" ></td> <td>${amount}</td> </tr>`;
+            var html = `<tr> <td>${name}</td>
+            <td><input type = "number" name="count" class="form-control" value="1" ></td>
+            <td>${amount}</td>
+            <td><button class="btn btn-danger btn-sm remove-product-btn"   data-id="${id}">
+               <span class="fa fa-trash"></span>
+               </button>
+            </td> </tr>`;
             document.querySelector('.order_list').insertAdjacentHTML('beforeend', html);
         });
+    });
+
+    document.querySelector('.order_list').addEventListener('click', function(event) {
+        if (event.target.closest('.remove-product-btn')) {
+            event.target.closest('tr').remove();
+        }
     });
 });
