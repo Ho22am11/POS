1a7bdccb8ea6c553f5dc33359c67117126113ec5
Hossam harraz <harrazhossam11@gmail.com>
2024-05-31 16:51:50 +0300

    edit customer controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,27 +6,33 @@ use App\Models\Customer;
 use Illuminate\Http\Request;
 use App\Http\Requests\CustomerRequest ;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class CustomerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    protected $Customer;
+
+    public function __construct(Customer $Customer)
+    {
+        return $this->Customer = $Customer ;
+    }
+
     public function index()
     {
-        $customers = Customer::all();
+        $customers = $this->Customer->all();
         return view('pages.customer.index' , compact('customers'));
     }
 
-    public function create()
-    {
-        //
-    }
 
     public function store(CustomerRequest $request)
     {
-        $request->validated();
+        DB::beginTransaction(); 
+
+        try {
+            $request->validated();
+        //frist create user for Customer
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -34,44 +40,45 @@ class CustomerController extends Controller
             'password' => Hash::make($request->password),
         ])->assignRole('user');
 
-        Customer::create([
-            'user_id' => $user->id ,
-            'addrese' => $request->addrese ,
-            'num' => $request->num ,
-        ]);
+
+        $data = $request->only(['addrese', 'num']);
+        //add value of id user for data 
+        $data['user_id'] = $user->id;
+
+        $this->Customer->create($data) ;
+
         return back()->with('success', 'Customer added successfully.');
+
+        DB::commit();
+
+        } catch(\Exception $e){
+            DB::rollBack();
+            return back()->with('error', $e->getMessage() );
+        }
+
+        
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Customer $customer)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit($id)
     {
-        $customer = Customer::findOrFail($id);      
+        
+        $customer = $this->Customer->findOrFail($id);      
         return view('pages.customer.edit' , compact('customer') ) ;
     }
 
     
     public function update(Request $request, $id )
     {
-        $customer = Customer::findOrFail($id);
+        try {
+
+        $customer = $this->Customer->findOrFail($id);
         $customer->update($request->all());
         return redirect()->route('customers.index');
+        
+        } catch(\Exception $e){
+            return back()->with('error', $e->getMessage() );
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Customer $customer)
-    {
-        //
-    }
 }
